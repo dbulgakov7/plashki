57cd593ce43615205069b0f7168773c5c5e5f540
Dmitry Bulgakov <dbulgakov7@yandex.ru>
2022-04-26 16:00:01 +0300

    build(1.0.1):

--- create_distrib.bat
+++ create_distrib.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\7-Zip\7z.exe" a plashki.7z exp2csv.bat LICENSE plashki.exe plashki.ini README.txt sqlite3.dll sqlite3.exe
+"C:\Program Files\7-Zip\7z.exe" a releases/plashki.7z exp2csv.bat LICENSE plashki.exe plashki.ini README.md sqlite3.dll sqlite3.exe
